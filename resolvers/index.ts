70603e634d8c4599d1635bd3f497580d925daf58
pfranczak <przemyslaw.franczak@volt.io>
2024-01-15 22:34:23 +0100

    Schema

--- resolvers/index.ts
+++ resolvers/index.ts
@@ -1,10 +1,13 @@
 import { resolvers as UserResolvers } from "./users.ts";
+import { resolvers as ReplacementResolvers } from "./replacements.ts";
 
 export const resolvers = {
     Query: {
-        ...UserResolvers.Query
+        ...UserResolvers.Query,
+        ...ReplacementResolvers.Query,
     },
     Mutation: {
-        ...UserResolvers.Mutation
+        ...UserResolvers.Mutation,
+        ...ReplacementResolvers.Mutation,
     },
 };
